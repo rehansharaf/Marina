de2cee1a8f864ff97a776fd282e45e900e64884d
talha.tahir <talha.tahir@PC-1F-032.appedology.internal>
2023-11-30 05:57:45 -0800

    update tc_1105 , the 2nd and 3rd bucket code added, tc_1106, is in progress, 1st , 2nd , and 3rd bucket result has get, remain work on  tc_1104 , tc_1105 and tc_1106 year dynamic.

--- src/test/java/com/marina/tests/Mod_7_RatesStorage.java
+++ src/test/java/com/marina/tests/Mod_7_RatesStorage.java
@@ -46,138 +46,138 @@ public class Mod_7_RatesStorage extends TestBase {
 
 	}
 
-//	@Test(groups = { "regression", "smoke" }, priority = 1, description = "Creating Rate Group from Space Type,"
-//			+ " check rate group should be available")
-//
-//
-//	public void createGroup_verifyGroup_TC_1101() throws InterruptedException {
-//
-//		Log.startTestCase("Creating Rate Group from Space Type, check rate group should be available Tc_1101");
-//		
-//		stp = hp.space_types_click_Mod7_ratesStorage();
-//		statp = stp.click_btn_add_space_type();
-//		stp = statp.adding_rates_group(rate_new_group_name);
-//		hp = stp.breadcrumbs_homePage();
-//		rsp = hp.spaces_dropdown_rates();
-//		boolean name =  rsp.rate_group_list(rate_new_group_name);
-//		Assert.assertTrue(name);
-//		Log.endTestCase("Creating Rate Group from Space Type, check rate group should be available Tc_1101");
-//
-//		
-//	}
+	@Test(groups = { "regression", "smoke" }, priority = 1, description = "Creating Rate Group from Space Type,"
+			+ " check rate group should be available")
+
+
+	public void createGroup_verifyGroup_TC_1101() throws InterruptedException {
+
+		Log.startTestCase("Creating Rate Group from Space Type, check rate group should be available Tc_1101");
+		
+		stp = hp.space_types_click_Mod7_ratesStorage();
+		statp = stp.click_btn_add_space_type();
+		stp = statp.adding_rates_group(rate_new_group_name);
+		hp = stp.breadcrumbs_homePage();
+		rsp = hp.spaces_dropdown_rates();
+		boolean name =  rsp.rate_group_list(rate_new_group_name);
+		Assert.assertTrue(name);
+		Log.endTestCase("Creating Rate Group from Space Type, check rate group should be available Tc_1101");
+
+		
+	}
 
 //	dependsOnMethods = "newlyGroupCreated_RevisionHisotryEmpty_TC_1101"
 
-//	@Test(groups = { "regression",
-//			"smoke" }, priority = 2, description = "No Revision History should be available in newly created rate group")
-//
-//	public void newlyGroupCreated_RevisionHisotryEmpty_TC_1102() throws InterruptedException {
-//
-//		Log.startTestCase("No Revision History should be available in newly created rate group Tc_1102");
-//	
-//		rsp.search_specifig_rates_group(rate_new_group_name);
-//		boolean[] expected_nightly_data = new boolean[2];
-//		expected_nightly_data[0] = true;
-//		
-//		expected_nightly_data[1] = true;
-//
-//		rsnpp = rsp.search_specifig_rates_group(rate_new_group_name);
-//		boolean[] recrod_nightly_data = rsnpp.newly_group_nightly_rivision_history();
-//
-//		rsmpp = rsnpp.single_click_monthly_price();
-//		boolean record_monthly_data = rsmpp.newly_group_monthly_rivision_history();
-//
-//		rsapp = rsmpp.single_click_annual_price();
-//		boolean record_annual_data = rsapp.newly_group_annual_rivision_history();
-//
-//		rsfpp = rsapp.single_click_flat_price();
-//		boolean record_flat_data = rsfpp.newly_group_annual_rivision_history();
-//
-//		Assert.assertEquals(recrod_nightly_data, expected_nightly_data);
-//		Assert.assertEquals(record_monthly_data, true);
-//		Assert.assertEquals(record_annual_data, true);
-//		Assert.assertEquals(record_flat_data, true);
-//
-//		Log.endTestCase("No Revision History should be available in newly created rate group Tc_1102");
-//
-//	}
+	@Test(groups = { "regression",
+			"smoke" }, priority = 2, description = "No Revision History should be available in newly created rate group")
+
+	public void newlyGroupCreated_RevisionHisotryEmpty_TC_1102() throws InterruptedException {
+
+		Log.startTestCase("No Revision History should be available in newly created rate group Tc_1102");
+	
+		rsp.search_specifig_rates_group(rate_new_group_name);
+		boolean[] expected_nightly_data = new boolean[2];
+		expected_nightly_data[0] = true;
+		
+		expected_nightly_data[1] = true;
+
+		rsnpp = rsp.search_specifig_rates_group(rate_new_group_name);
+		boolean[] recrod_nightly_data = rsnpp.newly_group_nightly_rivision_history();
+
+		rsmpp = rsnpp.single_click_monthly_price();
+		boolean record_monthly_data = rsmpp.newly_group_monthly_rivision_history();
+
+		rsapp = rsmpp.single_click_annual_price();
+		boolean record_annual_data = rsapp.newly_group_annual_rivision_history();
+
+		rsfpp = rsapp.single_click_flat_price();
+		boolean record_flat_data = rsfpp.newly_group_annual_rivision_history();
+
+		Assert.assertEquals(recrod_nightly_data, expected_nightly_data);
+		Assert.assertEquals(record_monthly_data, true);
+		Assert.assertEquals(record_annual_data, true);
+		Assert.assertEquals(record_flat_data, true);
+
+		Log.endTestCase("No Revision History should be available in newly created rate group Tc_1102");
+
+	}
 
 //	dependsOnMethods = "newlyGroupCreated_RevisionHisotryEmpty_TC_1101"
 
-//	@Test(groups = { "regression",
-//			"smoke" }, priority = 3, description = "Add New buckets and verify in specific rate group")
-//	public void nighlty_addNew_Bucket_andVerifyBucket_TC_1103() throws InterruptedException {
-//
-//		Log.startTestCase("Add New buckets and verify in specific rate group Tc_1103");
-//		rsnpp = rsp.search_specifig_rates_group(rate_new_group_name);
-//
-//		rsnpaup = rsnpp.click_update_btn_return_edit_page();
-//		rsnpaup.click_bucket_btn();
-//		rsnpp = rsnpaup.add_more_buckets();
-//		boolean newly_bucket_result = rsnpp.fouth_buckets_check();
-//
-//		Assert.assertEquals(newly_bucket_result, true);
-//
-//		Log.endTestCase("Add New buckets and verify in specific rate group Tc_1103");
-//
-//	}
+	@Test(groups = { "regression",
+			"smoke" }, priority = 3, description = "Add New buckets and verify in specific rate group")
+	public void nighlty_addNew_Bucket_andVerifyBucket_TC_1103() throws InterruptedException {
+
+		Log.startTestCase("Add New buckets and verify in specific rate group Tc_1103");
+		rsnpp = rsp.search_specifig_rates_group(rate_new_group_name);
+
+		rsnpaup = rsnpp.click_update_btn_return_edit_page();
+		rsnpaup.click_bucket_btn();
+		rsnpp = rsnpaup.add_more_buckets();
+		boolean newly_bucket_result = rsnpp.fouth_buckets_check();
+
+		Assert.assertEquals(newly_bucket_result, true);
+
+		Log.endTestCase("Add New buckets and verify in specific rate group Tc_1103");
+
+	}
 
 //	 dependsOnMethods = "nighlty_addNew_Bucket_andVerifyBucket_TC_1103"
 
-//	@Test(groups = { "regression",
-//			"smoke" }, priority = 4, description = "Delete newly added buckets and verify rate group")
-//	public void nightly_delete_newley_bucketAndVerify_TC_1104() throws InterruptedException {
-//
-//		
-//		Log.startTestCase("Delete newly added buckets and verify rate group TC_1104");
-//		rsnpp = rsp.search_specifig_rates_group(rate_new_group_name);
-//
-//		rsnpaup = rsnpp.click_update_btn_return_edit_page();
-//		rsnpaup.click_bucket_btn();
-//		rsnpp = rsnpaup.delete_newly_bucket();
-//		boolean newly_bucket_result = rsnpp.after_delete_fouthBucket_verify_delete();
-//
-//		Assert.assertEquals(newly_bucket_result, true);
-//
-//		Log.endTestCase("Delete newly added buckets and verify rate group TC_1104");
-//
-//	}
+	@Test(groups = { "regression",
+			"smoke" }, priority = 4, description = "Delete newly added buckets and verify rate group")
+	public void nightly_delete_newley_bucketAndVerify_TC_1104() throws InterruptedException {
+
+		
+		Log.startTestCase("Delete newly added buckets and verify rate group TC_1104");
+		rsnpp = rsp.search_specifig_rates_group(rate_new_group_name);
+
+		rsnpaup = rsnpp.click_update_btn_return_edit_page();
+		rsnpaup.click_bucket_btn();
+		rsnpp = rsnpaup.delete_newly_bucket();
+		boolean newly_bucket_result = rsnpp.after_delete_fouthBucket_verify_delete();
+
+		Assert.assertEquals(newly_bucket_result, true);
+
+		Log.endTestCase("Delete newly added buckets and verify rate group TC_1104");
+
+	}
 	
 	
 
 //	dependsOnMethods = "newlyGroupCreated_RevisionHisotryEmpty_TC_1101"
 
 	
-//	@Test(groups = { "regression",
-//			"smoke" }, priority = 5, description = "Create Nightly rate group with single row and without holiday")
-//	public void nightly_add_record_without_holiday_TC_1105() throws InterruptedException {
-//
-//		Log.startTestCase("Create Nightly rate group with single row and without holiday TC_1105");
-//
-//		rsnpp = rsp.search_specifig_rates_group(rate_new_group_name);
-//
-//		rsnpaup = rsnpp.click_update_btn_return_edit_page();
-//		boolean succesfuuly_add_nightly_price = rsnpaup.add_12_month_price_in_one_week_row();
-//
-//		Thread.sleep(1000);
-//
-//		rsnpaup = rsnpp.click_update_btn_return_edit_page();
-//		rsnpaup.single_click_second_bucket();
-//		boolean second_bucket_priced_updated = rsnpaup.add_12_month_price_in_one_week_row_second_bucket();
-//
-//		Thread.sleep(1000);
-//
-//		rsnpaup = rsnpp.click_update_btn_return_edit_page();
-//		rsnpaup.single_click_third_bucket();
-//		boolean third_bucket_priced_updated = rsnpaup.add_12_month_price_in_one_week_row_third_bucket();
-//
-//		Assert.assertEquals(succesfuuly_add_nightly_price, true);
-//		Assert.assertEquals(second_bucket_priced_updated, true);
-//		Assert.assertEquals(third_bucket_priced_updated, true);
-//
-//		Log.endTestCase("Create Nightly rate group with single row and without holiday TC_1105");
-//
-//	}
+	@Test(groups = { "regression",
+			"smoke" }, priority = 5, description = "Create Nightly rate group with single row and without holiday")
+	public void nightly_add_record_without_holiday_TC_1105() throws InterruptedException {
+
+		Log.startTestCase("Create Nightly rate group with single row and without holiday TC_1105");
+
+		rsnpp = rsp.search_specifig_rates_group(rate_new_group_name);
+
+		rsnpaup = rsnpp.click_update_btn_return_edit_page();
+		boolean succesfuuly_add_nightly_price = rsnpaup.add_12_month_price_in_one_week_row();
+
+		Thread.sleep(1000);
+
+		rsnpaup = rsnpp.click_update_btn_return_edit_page();
+		rsnpaup.single_click_second_bucket();
+		boolean second_bucket_priced_updated = rsnpaup.add_12_month_price_in_one_week_row_second_bucket();
+
+		Thread.sleep(1000);
+
+		rsnpaup = rsnpp.click_update_btn_return_edit_page();
+		rsnpaup.single_click_third_bucket();
+		boolean third_bucket_priced_updated = rsnpaup.add_12_month_price_in_one_week_row_third_bucket();
+
+		Assert.assertEquals(succesfuuly_add_nightly_price, true);
+		Assert.assertEquals(second_bucket_priced_updated, true);
+		Assert.assertEquals(third_bucket_priced_updated, true);
+
+		Log.endTestCase("Create Nightly rate group with single row and without holiday TC_1105");
+
+	}
 
 	
 	
